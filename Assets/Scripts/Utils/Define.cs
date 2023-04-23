a8e2dd65bb4c95d544c8ad3f18e663d81417a595
A6ly <a6ly26@gmail.com>
2023-04-24 02:17:45 +0900

    Add CommandType, DamageType

--- Assets/Scripts/Utils/Define.cs
+++ Assets/Scripts/Utils/Define.cs
@@ -1,5 +1,17 @@
 public class Define
 {
+    public enum CommandType
+    {
+        Move,
+        Attack,
+    }
+
+    public enum DamageType
+    {
+        Physical,
+        Magic,
+    }
+
     public enum Layer
     {
         Ground = 7,
