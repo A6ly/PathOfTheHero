ddbb843ac33c99369400c47f73df009b8903dbf2
A6ly <a6ly26@gmail.com>
2023-04-24 02:30:47 +0900

    Fix grid object selector feature

--- Assets/GridObjectSelector.cs
+++ Assets/GridObjectSelector.cs
@@ -4,16 +4,22 @@ public class GridObjectSelector : MonoBehaviour
 {
     GridMousePointer gridMousePointer;
     CommandMenu commandMenu;
+    StatusWindow statusWindow;
+
     private void Awake()
     {
         gridMousePointer = GetComponent<GridMousePointer>();
         commandMenu = GetComponent<CommandMenu>();
+        statusWindow = GetComponent<StatusWindow>();
     }
 
-    public GridObject hoverOverGridObject;
-    public GridObject selected;
+    public Character selected;
+    public Character hoverOverCharacter;
 
+    GridObject hoverOverGridObject;
     Grid targetGrid;
+
+    bool isSelected;
 
     Vector2Int positionOnGrid = new Vector2Int(-1, -1);
 
@@ -33,28 +39,43 @@ public class GridObjectSelector : MonoBehaviour
         DeselectObject();
     }
 
+    private void LateUpdate()
+    {
+        if (selected != null)
+        {
+            if (isSelected == false)
+            {
+                selected = null;
+            }
+        }
+    }
+
     private void OnMouseOverObject()
     {
         positionOnGrid = gridMousePointer.positionOnGrid;
         hoverOverGridObject = targetGrid.GetPlacedObject(positionOnGrid);
+
+        if (hoverOverGridObject != null)
+        {
+            hoverOverCharacter = hoverOverGridObject.GetComponent<Character>();
+        }
+        else
+        {
+            hoverOverCharacter = null;
+        }
     }
 
     private void SelectObject()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (hoverOverGridObject != null)
+            if (hoverOverCharacter != null && selected == null)
             {
-                selected = hoverOverGridObject;
+                selected = hoverOverCharacter;
+                isSelected = true;
 
-                if (selected.CompareTag("Character"))
-                {
-                    commandMenu.OpenPanel(selected.GetComponent<CharacterTurn>());
-                }
-                else if (selected.CompareTag("Monster"))
-                {
-
-                }
+                commandMenu.OpenMenu(selected.GetComponent<CharacterTurn>());
+                statusWindow.OpenWindow(selected.GetComponent<Character>());
             }
         }
     }
@@ -64,6 +85,14 @@ public class GridObjectSelector : MonoBehaviour
         if (Input.GetMouseButtonDown(1))
         {
             selected = null;
+
+            commandMenu.CloseMenu();
+            statusWindow.CloseWindow();
         }
+    }
+
+    public void Deselect()
+    {
+        isSelected = false;
     }
 }
