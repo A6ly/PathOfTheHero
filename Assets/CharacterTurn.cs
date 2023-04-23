4676cab1585a3401d7b44312a2fe012e484106fe
A6ly <a6ly26@gmail.com>
2023-04-24 02:02:51 +0900

    Add character turn feature

--- Assets/CharacterTurn.cs
+++ Assets/CharacterTurn.cs
@@ -2,5 +2,17 @@ using UnityEngine;
 
 public class CharacterTurn : MonoBehaviour
 {
+    public bool canMove;
+    public bool canAttack;
 
+    private void Start()
+    {
+        ResetTurn();
+    }
+
+    public void ResetTurn()
+    {
+        canMove = true;
+        canAttack = true;
+    }
 }
